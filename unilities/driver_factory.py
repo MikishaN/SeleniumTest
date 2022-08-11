d740dc9589a96ff46d3a25ceabeadd50a5526a86
Natalya Pesnia <n.mikisha@gmail.com>
2022-08-11 23:26:29 +0200

    test

--- unilities/driver_factory.py
+++ unilities/driver_factory.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -15,7 +14,7 @@ class DriverFactory:
             chrome_options = Options()
             if is_headless:
                 chrome_options.add_argument("--headless")
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+            driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
             # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
         else:
