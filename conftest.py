59b44481267807786f3699a7aed6b0f392cb7823
MikishaN <n.mikisha@gmail.com>
2022-08-12 22:14:40 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -15,6 +15,6 @@ def create_driver():
 @pytest.fixture()
 def open_main_page(create_driver):
     driver = create_driver
-    driver.get('https://www.biblio.com/'')
+    driver.get('https://www.biblio.com/')
     main_page = MainPage(driver)
     return main_page
