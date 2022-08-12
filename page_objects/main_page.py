07133679901b5bd45c394fe18719ae7a44beaf37
MikishaN <n.mikisha@gmail.com>
2022-08-12 22:14:10 +0200

    Update main_page.py

--- page_objects/main_page.py
+++ page_objects/main_page.py
@@ -6,7 +6,7 @@ from unilities.web_ui.base_page import BasePage
 
 
 class MainPage(BasePage):
-    __page_title = (By.XPATH, '//div[@class="imin-section"]/div[@class="imin-container"]/a')
+    __page_title = (By.XPATH, '//section[@id="biblio-favorites"]/h2')
 
     def __init__(self, driver):
         super().__init__(driver)
